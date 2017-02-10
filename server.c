6f6d8725fcce3bd9d5bb651389dec6c7442ff1e7
Science <565369157@qq.com>
2017-02-10 22:17:43 +0800

    Socket communication problem of different length structures using UDP for different byte length platforms.

--- server.c
+++ server.c
@@ -11,24 +11,55 @@
 #define SERVER_PORT 6666
 #define MAX_MSG 1024
 
+char *os = {"Ubuntu"};
+unsigned int CS_counter;
+
+#pragma pack(1)
+typedef struct DATA{
+  int nodeSize;
+  char os_name[20];
+  unsigned int num;
+}UDP_data;
+#pragma pack()
+
 void udps_respon(int sockfd)
 {
     struct sockaddr_in addr;
-    int nbytes;
+    int nbytes, tmplen, size_UDP_data;
     socklen_t size;
     char message[MAX_MSG];
+    UDP_data *S_Msg, *R_Msg;
 
     while(1)
     {
         size = sizeof(struct sockaddr_in);
-        nbytes = recvfrom(sockfd,message,MAX_MSG,0,(struct sockaddr *)&addr,&size);
+        nbytes = recvfrom(sockfd, &message, MAX_MSG, 0, (struct sockaddr *)&addr, &size);
         if(nbytes < 0)
         {
           printf("recvfrom(server) failed.\n");
           exit(1);
         }
-        printf("Server got message %s from IP->%s :%d\n", message, inet_ntoa(addr.sin_addr) ,ntohs(addr.sin_port));
-        nbytes = sendto(sockfd,message,nbytes,0,(struct sockaddr*)&addr,size);
+
+        R_Msg = (UDP_data *)malloc(sizeof(UDP_data));
+
+        memcpy(R_Msg, message, sizeof(UDP_data));
+        CS_counter = R_Msg->num;
+        CS_counter++;
+        if((CS_counter%2) == 0)
+        {
+          exit(1);
+        }
+
+        printf("----------------------------------------nbytes:%d    size=%d\n", nbytes, size);    //
+        printf("Server got message : {OS_name : %s ; num : %d} from IP->%s :%d\n", R_Msg->os_name, CS_counter, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+        S_Msg = (UDP_data *)malloc(sizeof(UDP_data));
+        S_Msg->nodeSize = sizeof(UDP_data);
+        strcpy(S_Msg->os_name, os);
+
+        S_Msg->num = CS_counter;
+
+        nbytes = sendto(sockfd,(char *)S_Msg, S_Msg->nodeSize, 0, (struct sockaddr*)&addr, size);
         if(nbytes < 0)
         {
           printf("sendto(server) failed.\n");
